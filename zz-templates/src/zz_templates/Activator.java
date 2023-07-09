f5a43cd9f3b48ed4be1b92b518ce401711f5049f
goga <goga@GOGAL-LIN>
2023-07-09 22:42:12 +0400

    2023-07-09-reorg-*

---
+++ zz-templates/src/zz_templates/Activator.java
@@ -0,0 +1,22 @@
+package zz_templates;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+	}
+
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+	}
+
+}
---
+++ zz-templates/src/zz_templates/l10n/ITsResources.java
@@ -0,0 +1,13 @@
+package zz_templates.l10n;
+
+/**
+ * Localizable resources.
+ *
+ * @author hazard157
+ */
+interface ITsResources {
+
+  String STR_XXX   = Messages.getString( "STR_XXX" );   //$NON-NLS-1$
+  String STR_XXX_D = Messages.getString( "STR_XXX_D" ); //$NON-NLS-1$
+
+}
